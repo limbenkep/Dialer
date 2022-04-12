98f3db82a9dc1dc768d7c5cba066cf5b9dd0bd14
Lima Honorine <holi1900@student.miun.se>
2022-04-12 19:33:17 +0200

    add functionality in MainActivity to keep track if the user has already opened the About dialog during the current life cycle of the activity.

--- app/src/main/java/se/miun/holi1900/dt031g/dialer/MainActivity.java
+++ app/src/main/java/se/miun/holi1900/dt031g/dialer/MainActivity.java
@@ -1,17 +1,71 @@
 package se.miun.holi1900.dt031g.dialer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //variable that keeps track of the state of the About page. Set to tru when the about button is clicked
+    private static final String ABOUT_PAGE_STATE = "aboutPageOpened";
+    //Tag for logging
+    private static final String TAG= "assignment4";
+    private boolean aboutPageOpened;
+    private Button about;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get the reference to the about button that opens about activity on click
+        about = findViewById(R.id.aboutButton);
+
+        // Check whether we're recreating a previously destroyed instance
+        if (savedInstanceState != null) {
+            // Restore value of saved state (dice value)
+            aboutPageOpened = savedInstanceState.getBoolean(ABOUT_PAGE_STATE); // Returns the value associated with the given key, or 0
+            Log.d(TAG, "onCreate: we have a saved instance state, aboutPageOpened=" + aboutPageOpened);
+        } else {
+            Log.d(TAG, "onCreate: no saved instance state");
+        }
+
+    }
+
+    /**
+     * The onSaveInstanceState() method is called when an activity starts to stop unexpectedly,so the
+     * activity can save state information (key-value pairs) to an instance state bundle. The state
+     * can then be restored in onCreate or onRestoreInstanceState (the Bundle populated by this
+     * method will be passed to both).
+     *
+     * @param outState Bundle in which to place your saved state (aboutPageOpened value).
+     */
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putBoolean(ABOUT_PAGE_STATE, aboutPageOpened);
+        super.onSaveInstanceState(outState);
+    }
+
+    /**
+     * Instead of restoring the state during onCreate() you may choose to implement
+     * onRestoreInstanceState(), which the system calls after the onStart() method. The system
+     * calls onRestoreInstanceState() only if there is a saved state to restore, so you do not
+     * need to check whether the Bundle is null.
+     *
+     * @param savedInstanceState the data most recently supplied in onSaveInstanceState
+     */
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        //the superclass restore the view hierarchy
+        super.onRestoreInstanceState(savedInstanceState);
+        //Restore value of for aboutPageState that tracks state of About activity
+        aboutPageOpened= savedInstanceState.getBoolean(ABOUT_PAGE_STATE);
     }
 
     /**Called when the DIAL button is clicked*/
@@ -36,8 +90,19 @@ public class MainActivity extends AppCompatActivity {
 
     /**Called when the About button is clicked*/
     public void openAboutDialog(View view){
-        AboutDialog aboutDialog = new AboutDialog();
-        aboutDialog.show(getSupportFragmentManager(), "about dialog");
+        if(!aboutPageOpened){
+            aboutPageOpened = true;
+            AboutDialog aboutDialog = new AboutDialog();
+            aboutDialog.show(getSupportFragmentManager(), "about dialog");
+        }else{
+            Context context = getApplicationContext();
+            //text to be displayed to user
+            CharSequence text = "Dialog already seen!";
+            //Duration the toast should remain on the screen
+            int duration = Toast.LENGTH_SHORT;
+            //The makeText() method returns a properly initialized Toast
+            //object and show() method display toast
+            Toast.makeText(context, text, duration).show();
+        }
     }
-
 }
